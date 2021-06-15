e7b4b7675a85b23c4b7e839117d3a5c4f6750bb0
Christiano0407 <placebo0704@gmail.com>
2021-06-15 09:45:01 -0500

    image-rename

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
-let image = [...document.querySelectorAll(`.image`)]; 
+let images = [...document.querySelectorAll(`.img`)]; 
+let slider = document.querySelector(`.slider`); 
+let sliderWidth; 
+let imageWidth;
+let current = 0; 
+let target = 0; 
+let ease = .05;  
+
+images.forEach((img, idx) => {
+  img.style.backgroundImage = `url(../assets/image/${idx+1}.jpg)` 
+}); 
