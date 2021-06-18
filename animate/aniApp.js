c4f70029f31316c01da6007802434810dd4c201d
Christiano0407 <placebo0704@gmail.com>
2021-06-18 10:15:15 -0500

    practice01

--- animate/aniApp.js
+++ animate/aniApp.js
@@ -1,1 +1,8 @@
 console.log("Animation Scoll"); 
+
+let aosElements = Array.from(document.querySelectorAll(`.aos`));
+//console.log(aosElements); 
+
+aosElements.forEach(element => {
+    console.log(element.getBoundingClientRect()); 
+})
