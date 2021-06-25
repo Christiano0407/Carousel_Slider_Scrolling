5e20471b6f94dda3e4261073ec9bdd3a52c33794
Christiano0407 <placebo0704@gmail.com>
2021-06-25 08:33:11 -0500

    scrollBar03

--- scroll/appJsStocky.js
+++ scroll/appJsStocky.js
@@ -1,1 +1,25 @@
-console.log("Scroll"); 
+//console.log("Scroll"); 
+const scrollAble = document.querySelector(`.scrollBar`); 
+
+let current = 0;
+let target = 0; 
+const ease = 0.1; 
+// Nos concentramos en los parámetros para el scroll.
+function up(start, end, t)
+{
+    return start * ( 1 - t) + end * t; 
+}
+//Modificar la altura del body.
+init = () => {
+    document.body.style.height = `${scrollAble.getBoundingClientRect().height}px`; 
+}
+//scroll en Y:
+function smoothScroll()
+{
+   target = window.scrollY; 
+   current = up(current, target, ease); 
+   scrollAble.style.transform =`translate3d(0, ${-current}px, 0)`; 
+}
+
+init(); 
+smoothScroll();
