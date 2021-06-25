774fe06cd65bdc8337df8572f3d72caefab4eed6
Christiano0407 <placebo0704@gmail.com>
2021-06-25 09:04:49 -0500

    scroll04

--- scroll/appJsStocky.js
+++ scroll/appJsStocky.js
@@ -1,5 +1,6 @@
 //console.log("Scroll"); 
 const scrollAble = document.querySelector(`.scrollBar`); 
+const stickyProject = document.querySelector(`.project`); 
 
 let current = 0;
 let target = 0; 
@@ -19,7 +20,21 @@ function smoothScroll()
    target = window.scrollY; 
    current = up(current, target, ease); 
    scrollAble.style.transform =`translate3d(0, ${-current}px, 0)`; 
+   sticky(); 
    window.requestAnimationFrame(smoothScroll);
+}
+
+sticky = () => {
+   let offset = window.innerHeight * 2; 
+
+   if(sticky < offset) 
+   {
+      stickyProject.style.transform = `translate3d(0, 0, 0)`; 
+   }
+   if(current >= offset)
+   {
+       stickyProject.style.transform = `translate3d(0, ${current - offset}px, 0)`; 
+   }
 }
 
 init(); 
